8d32de5b5c5511796c57a5e2b8d23c727c6ee50b
Anthony Green <green@moxielogic.com>
2018-09-05 07:13:25 -0400

    User vault for secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ -z "$MINIO_ACCESS_KEY" ]; then
-    echo ERROR: MINIO_ACCESS_KEY environment variable missing.  Create this secret in OpenShift.
+set -x
+
+if [ -z "$VAULT_MINIO_ACCESS_TOKEN" ]; then
+    echo ERROR: VAULT_MINIO_ACCESS_TOKEN environment variable missing.  Create this secret in OpenShift.
     exit 1
 fi
-    
-if [ -z "$MINIO_SECRET_KEY" ]; then
-    echo ERROR: MINIO_SECRET_KEY environment variable missing.  Create this secret in OpenShift.
-    exit 1
-fi
-    
+
+SECRETS=`curl -H "X-Vault-Token: $VAULT_MINIO_ACCESS_TOKEN" -X GET http://vault34-vault34.apps.home.labdroid.net/v1/secret/minio`
+
+export MINIO_ACCESS_KEY=`echo $SECRETS | jq .data.MINIO_ACCESS_KEY`
+export MINIO_SECRET_KEY=`echo $SECRETS | jq .data.MINIO_SECRET_KEY`
+
 HOME="`pwd`"
 $HOME/minio server --config-dir=$HOME/config $@ $HOME/data
