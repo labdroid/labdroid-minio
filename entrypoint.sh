d35a04f12ac2ef9bb44e72de07b53bbafaff3ad0
Anthony Green <green@moxielogic.com>
2018-09-05 08:59:16 -0400

    Use vault name for service

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "$VAULT_MINIO_READ_ACCESS_TOKEN" ]; then
     exit 1
 fi
 
-SECRETS=`curl -H "X-Vault-Token: $VAULT_MINIO_READ_ACCESS_TOKEN" -X GET http://vault34-vault34.apps.home.labdroid.net/v1/secret/minio`
+SECRETS=`curl -H "X-Vault-Token: $VAULT_MINIO_READ_ACCESS_TOKEN" -X GET http://vault/v1/secret/minio`
 
 export MINIO_ACCESS_KEY=`echo $SECRETS | jq .data.MINIO_ACCESS_KEY`
 export MINIO_SECRET_KEY=`echo $SECRETS | jq .data.MINIO_SECRET_KEY`
