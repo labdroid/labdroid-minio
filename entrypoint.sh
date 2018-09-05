3c9cf9ac8e726e590b33ead3e3903c13c8f9bda4
Anthony Green <green@moxielogic.com>
2018-09-05 07:15:54 -0400

    Change token var name:

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-if [ -z "$VAULT_MINIO_ACCESS_TOKEN" ]; then
-    echo ERROR: VAULT_MINIO_ACCESS_TOKEN environment variable missing.  Create this secret in OpenShift.
+if [ -z "$VAULT_MINIO_READ_ACCESS_TOKEN" ]; then
+    echo ERROR: VAULT_MINIO_READ_ACCESS_TOKEN environment variable missing.  Create this secret in OpenShift.
     exit 1
 fi
 
-SECRETS=`curl -H "X-Vault-Token: $VAULT_MINIO_ACCESS_TOKEN" -X GET http://vault34-vault34.apps.home.labdroid.net/v1/secret/minio`
+SECRETS=`curl -H "X-Vault-Token: $VAULT_MINIO_READ_ACCESS_TOKEN" -X GET http://vault34-vault34.apps.home.labdroid.net/v1/secret/minio`
 
 export MINIO_ACCESS_KEY=`echo $SECRETS | jq .data.MINIO_ACCESS_KEY`
 export MINIO_SECRET_KEY=`echo $SECRETS | jq .data.MINIO_SECRET_KEY`
